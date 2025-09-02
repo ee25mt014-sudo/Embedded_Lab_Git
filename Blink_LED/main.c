855fa421c9e25a286ec74b1c3b4a55c117240524
Bharath Naragund <ee25mt014@iitdh.ac.in>
2025-09-02 13:06:02 +0530

    Main

--- Blink_LED/main.c
+++ Blink_LED/main.c
@@ -1,9 +1,20 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
 
+int main(void) {
+    volatile unsigned int i;                  // Declare delay variable
 
-/**
- * main.c
- */
-int main(void)
-{
-	return 0;
+    SYSCTL_RCGCGPIO_R |= 0x20;                // Enable clock to Port F
+    while ((SYSCTL_PRGPIO_R & 0x20) == 0);    // Wait until Port F is ready
+
+    GPIO_PORTF_DIR_R |= 0x08;                 // PF1 (Red LED) as output
+    GPIO_PORTF_DEN_R |= 0x08;                 // Enable digital on PF1
+
+    while (1) {
+        GPIO_PORTF_DATA_R |= 0x08;            // Turn ON Red LED
+        for (i = 0; i < 500000; i++);         // Delay
+
+        GPIO_PORTF_DATA_R &= ~0x08;           // Turn OFF Red LED
+        for (i = 0; i < 500000; i++);         // Delay
+    }
 }
